64293a93ab2ec0986535fda63011eb3f26ff128f
Patrick <Pjwelch1230@gmail.com>
2024-08-05 14:25:17 -0400

    Implemented random rgb and increasing opacity

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ container.style.width="fit-content";
 container.style.marginRight="auto";
 container.style.marginLeft="auto";
 container.style.flexWrap="nowrap";
+
+let opa = 0.01;
+
 for (let i = 0; i < 16; i++) {
     const xSquare = document.createElement("div");
     xSquare.style.display="inline-block";
     xSquare.style.flexDirection="column";
     xSquare.style.flexWrap="nowrap";
     for (let j = 0; j < 16; j++) {
+        let entered = false;
         const ySquare = document.createElement("div");
         if (i === 15 && j != 15) {
             ySquare.classList.add("rightB");
@@ -22,9 +26,17 @@ for (let i = 0; i < 16; i++) {
         }  else {
             ySquare.classList.add("square");
         }
+        const r = Math.floor(Math.random() * 256);
+        const g= Math.floor(Math.random() * 256);
+        const b= Math.floor(Math.random() * 256);
         ySquare.style.flexWrap="nowrap";
         ySquare.addEventListener("mouseenter", () => {
-            ySquare.style.backgroundColor = "black";
+            if (!entered) {
+                ySquare.style.backgroundColor = "rgb("+r+","+g+","+b+","+opa+")";
+                opa < 0.99 ? opa += 0.1 : opa = 1;
+                console.log(opa);
+            }
+            entered = true;
         });
         xSquare.appendChild(ySquare);
     }
