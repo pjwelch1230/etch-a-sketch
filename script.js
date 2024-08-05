64f995f1e5b0149f6842db8b354daa9eea3617ba
Patrick <Pjwelch1230@gmail.com>
2024-08-05 13:34:55 -0400

    Grid prints and hover-effect is functional

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ const container = document.querySelector("#container");
 
 container.style.display="flexbox";
 container.style.flexDirection="row";
+container.style.width="fit-content";
+container.style.marginRight="auto";
+container.style.marginLeft="auto";
+container.style.flexWrap="nowrap";
 for (let i = 0; i < 16; i++) {
     const xSquare = document.createElement("div");
-    // if (i == 15) {
-    //     xSquare.classList.add("rightB");
-    // }  else {
-    //     xSquare.classList.add("square");
-    // }
     xSquare.style.display="inline-block";
     xSquare.style.flexDirection="column";
+    xSquare.style.flexWrap="nowrap";
     for (let j = 0; j < 16; j++) {
         const ySquare = document.createElement("div");
-        i === 15 ?
-            ySquare.classList.add("rightB") : ySquare.classList.add("square");        
-        // if (j === 15) {
-        //     ySquare.classList.add("bottomB");
-        // }
+        if (i === 15 && j != 15) {
+            ySquare.classList.add("rightB");
+        }  else if (i === 15 & j === 15) {
+            ySquare.classList.add("bottomR");
+        }  else if (i != 15 & j === 15) {
+            ySquare.classList.add("bottom");
+        }  else {
+            ySquare.classList.add("square");
+        }
+        ySquare.style.flexWrap="nowrap";
+        ySquare.addEventListener("mouseenter", () => {
+            ySquare.style.backgroundColor = "black";
+        });
         xSquare.appendChild(ySquare);
     }
     container.appendChild(xSquare);
